ee322b31c03ba0c090e2624b53ae3ad0589f7a14
Muhammad Saad Mateen <saad.mateen@hotmail.com>
2016-02-11 19:49:30 +0000

    Added a new test for positive number

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -3,6 +3,11 @@ var expect = chai.expect;
 var validator = require('../lib/validator.js');
 
 describe('A Validator', function(){
+
+	it('will return no errors for valid number', function () {
+		expect(validator(3)).to.be.empty;
+	});
+
 	it('will return error.nonpositive for not strictly positive numbers', function() {
 		expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
 	});
