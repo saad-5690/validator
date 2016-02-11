516bfbbcefea101147d6ee87e512895a24e621d4
Muhammad Saad Mateen <saad.mateen@hotmail.com>
2016-02-11 20:27:16 +0000

    Added a nested test for divisible by 3

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -17,4 +17,14 @@ describe('A Validator', function(){
 			expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
 		});
 	});
+
+	describe('will return error.three for divisible by 3 numbers:', function() {
+		it('like 3', function() {
+			expect(validator(3)).to.be.deep.equal(['error.three']);
+		});
+		it('like 6', function() {
+			expect(validator(6)).to.be.deep.equal(['error.three']);
+		});
+	});
+
 });
