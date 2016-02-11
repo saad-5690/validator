a63ccdf12723df5e7f5b3e33cda6a015ca3a9a3a
Muhammad Saad Mateen <saad.mateen@hotmail.com>
2016-02-11 19:22:25 +0000

    Changed the test style to should

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-
-validator = require('../lib/validator.js')
+var chai = require('chai');
+var should = chai.should();
+var validator = require('../lib/validator.js');
 
 describe('A Validator', function(){
 	it('will return error.nonpositive for not strictly positive numbers', function() {
-		assert.deepEqual(validator(0), ['error.nonpositive']);
+		validator(0).should.to.be.deep.equal(['error.nonpositive']);
 	});
 });
