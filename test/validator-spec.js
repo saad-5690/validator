64ea077563cd24610ea0f44b69595f50722f3baf
Muhammad Saad Mateen <saad.mateen@hotmail.com>
2016-02-11 20:12:37 +0000

    Added a new test for negative numbers

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -8,7 +8,11 @@ describe('A Validator', function(){
 		expect(validator(3)).to.be.empty;
 	});
 
-	it('will return error.nonpositive for not strictly positive numbers', function() {
+	it('will return error.nonpositive for not strictly positive numbers, like 0', function() {
 		expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+	});
+
+	it('will return error.nonpositive for not strictly positive numbers, like -2', function() {
+		expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
 	});
 });
